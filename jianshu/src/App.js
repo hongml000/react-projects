f68ec2371edaad6341bec0a7b12058df1aa15c67
hongml000 <286995954@qq.com>
2020-04-29 21:09:43 +0800

    init project

---
+++ jianshu/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { GlobalStyled } from './style.js';
+import Header from './common/header'
+function App() {
+  return (
+    <div className="App">
+      <GlobalStyled />
+      {/* <Header><Header /> */}
+    </div>
+  );
+}
+
+export default App;
